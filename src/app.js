153bfb66d238914a77f6e316839ebe6aa5983ff5
adjeielias90 <adjeielias@gmail.com>
2019-05-13 10:33:37 +0000

    Add eslint

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-//create a server to listen on a given port
-const port = process.env.PORT || 9090
+// create a server to listen on a given port
+const port = process.env.PORT || 9090;
+
 app.listen(port, () => {
   console.log('The app started on port', port);
-})
-
+});
